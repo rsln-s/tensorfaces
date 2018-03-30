f4b375840021dd296ada6163e74f4d8ef92affa1
Ruslan Shaydulin <ruslan@l2sh.ru>
2018-03-29 22:44:02 -0400

    now correctly parses image filename into three convenient intergers and a string id

--- tensor/tensorlab_playground.m
+++ tensor/tensorlab_playground.m
@@ -1,4 +1,25 @@
-imdata = imread('FaceBase_png/meir1-vp0-il0-ex2.png');
-J = rgb2gray(imdata);
-size(J)
-imshow(J)
+dirpath = 'FaceBase_warped/*.png';
+assumed_size_of_image = [110,115];
+
+imfiles = dir(dirpath);
+for file = imfiles'
+    imdata = imread(strcat('FaceBase_warped/', file.name));
+    if ~isequal(size(imdata), assumed_size_of_image)
+        disp('Error: image of incorrect size encountered')
+        return
+    end
+    idents = strsplit(file.name, {'-', '.'});
+    ident = idents(1);
+    
+    strids = regexp(idents(2:end-1),'[0-9]+', 'match');
+    if ~isequal(size(strids), [1,3])
+        disp('Error: incorrect file name')
+        return
+    end
+    vp = str2num(char(strids{1}))
+    ill = str2num(char(strids{2}))
+    ex = str2num(char(strids{3}))
+    
+%     imshow(imdata)
+    break
+end
