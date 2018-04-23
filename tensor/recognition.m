670dd0c140761d2d8c81c9d537be7ff3cc06c272
Ruslan Shaydulin <ruslan@l2sh.ru>
2018-04-23 16:50:20 -0400

    experiment setup

--- tensor/recognition.m
+++ tensor/recognition.m
@@ -7,41 +7,81 @@ B = tmprod(B, U{3},3);
 B = tmprod(B, U{4},4);
 B = tmprod(B, U{5},5);
 
-minNorm = realmax;
-minInd = -1;
-minIll = -1;
-minVp = 1;
+% for all images in testing set
 
-imdata = im2double(imread('FaceBase_warped/bruno-vp2-il1-ex2.png'));
-flattened_imdata = imdata(imdata~=0);
-d = flattened_imdata / norm(flattened_imdata);
+testpath = 'test/*.png';
+testfiles = dir(testpath);
+size(testfiles)
 
-for ill = drange(1, num_ills)
-    for vp = drange(1, num_vps)
-        Bvie = B(:,vp,ill,1,:);
-        Bvie = reshape(Bvie, 28, []);
-        Bvieinv = pinv(Bvie');
+correct = 0;
+incorrect = 0;
 
-        % iterate through all c to find the one
-        % with the smallest norm of the difference
-        
-        % project d into a set of candidate vectors cvie
-        cvie = Bvieinv * d;
+for file = testfiles'
+    minNorm = realmax;
+    minInd = -1;
+    minIll = -1;
+    minVp = 1;
 
-        for i = drange(1,28)
-            diff = cvie' - U{1}(i,:);
-            currNorm = norm(diff);
-            if currNorm < minNorm
-                minInd = i;
-                minIll = ill;
-                minVp = vp;
-                minNorm = currNorm;
-            end
-        end  
+    imdata = im2double(imread(strcat('test/', file.name)));
+    if ~isequal(size(imdata), assumed_size_of_image)
+        disp('Error: image of incorrect size encountered')
+        return
     end
-end
+    
+    flattened_imdata = imdata(imdata~=0);
+    d = flattened_imdata / norm(flattened_imdata);
+    
+    idents = strsplit(file.name, {'-', '.'});
+    ident = idents(1);
+    person_ind = -1;
+    if ismember(ident, people)
+        person_ind = find(ismember(people, ident));
+        if ~isequal(size(person_ind), [1,1])
+            disp('Oooops, something went wrong with people cell array')
+            return
+        end
+        person_ind = person_ind(1);
+    else
+        disp('Error: new person encountered')
+    end
+    
+    for ill = drange(1, num_ills)
+        for vp = drange(1, num_vps)
+            Bvie = B(:,vp,ill,1,:);
+            Bvie = reshape(Bvie, 28, []);
+            Bvieinv = pinv(Bvie');
 
-disp(minInd);
-disp(minVp);
-disp(minIll);
-disp(minNorm);
+            % iterate through all c to find the one
+            % with the smallest norm of the difference
+
+            % project d into a set of candidate vectors cvie
+            cvie = Bvieinv * d;
+
+            for i = drange(1,28)
+                diff = cvie' - U{1}(i,:);
+                currNorm = norm(diff);
+                if currNorm < minNorm
+                    minInd = i;
+                    minIll = ill;
+                    minVp = vp;
+                    minNorm = currNorm;
+                end
+            end  
+        end
+    end
+    if minInd == person_ind
+        correct = correct + 1;
+        disp(strcat('Correctly predicted for ', file.name))
+    else
+        incorrect = incorrect + 1;
+        disp(strcat('Incorrectly predicted for ', file.name))
+        disp(minInd)
+        disp(person_ind)
+    end
+%     disp(minInd);
+%     disp(minVp);
+%     disp(minIll);
+%     disp(minNorm);
+end
+disp(correct)
+disp(incorrect)
