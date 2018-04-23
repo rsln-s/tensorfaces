94977361047c131c181665bd83034980044ee4c6
Ruslan Shaydulin <ruslan@l2sh.ru>
2018-04-23 16:31:45 -0400

    added search over all illuminations & poses

--- tensor/recognition.m
+++ tensor/recognition.m
@@ -6,31 +6,42 @@ B = tmprod(S,U{2},2);
 B = tmprod(B, U{3},3);
 B = tmprod(B, U{4},4);
 B = tmprod(B, U{5},5);
-Bvie = B(:,1,1,1,:);
-Bvie = reshape(Bvie, 28, []);
-Bvieinv = pinv(Bvie');
-size(Bvieinv)
-
-% step two: read in the image and iterate through all c to find the one
-% with the smallest norm of the difference
-
-imdata = im2double(imread('FaceBase_warped/amir1-vp0-il0-ex2.png'));
-flattened_imdata = imdata(imdata~=0);
-d = flattened_imdata / norm(flattened_imdata);
-
-% project d into a set of candidate vectors cvie
-cvie = Bvieinv * d;
 
 minNorm = realmax;
 minInd = -1;
-for i = drange(1,28)
-    diff = cvie' - U{1}(i,:);
-    currNorm = norm(diff);
-    if currNorm < minNorm
-        minInd = i;
-        minNorm = currNorm;
+minIll = -1;
+minVp = 1;
+
+imdata = im2double(imread('FaceBase_warped/bruno-vp2-il1-ex2.png'));
+flattened_imdata = imdata(imdata~=0);
+d = flattened_imdata / norm(flattened_imdata);
+
+for ill = drange(1, num_ills)
+    for vp = drange(1, num_vps)
+        Bvie = B(:,vp,ill,1,:);
+        Bvie = reshape(Bvie, 28, []);
+        Bvieinv = pinv(Bvie');
+
+        % iterate through all c to find the one
+        % with the smallest norm of the difference
+        
+        % project d into a set of candidate vectors cvie
+        cvie = Bvieinv * d;
+
+        for i = drange(1,28)
+            diff = cvie' - U{1}(i,:);
+            currNorm = norm(diff);
+            if currNorm < minNorm
+                minInd = i;
+                minIll = ill;
+                minVp = vp;
+                minNorm = currNorm;
+            end
+        end  
     end
 end
 
 disp(minInd);
+disp(minVp);
+disp(minIll);
 disp(minNorm);
